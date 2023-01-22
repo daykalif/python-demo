c3b50aa913344117299b4e05218d0b9eb125a1a5
daykalif <1038200595@qq.com>
2023-01-22 21:34:10 +0800

    feat:filter,get,exclude的查询

--- DjangoDemo/bookmanager/book/views.py
+++ DjangoDemo/bookmanager/book/views.py
@@ -134,3 +134,61 @@ BookInfo.objects.all()
 # 获取count的两种方式：
 BookInfo.objects.all().count()
 BookInfo.objects.count()
+
+##################################filter,get,exclude###########################################
+
+
+"""
+select xxx from bookinfo where 条件语句
+相当于 where查询
+
+filter  :   筛选/过滤 返回n个结果（n=0/1/n）
+get     :           返回1个结果
+exclude :   排除掉符合条件剩下的结果 相当于 not
+
+语法形式：
+    以 filter(字段名__运算符=值) 为例
+"""
+
+# --------------------------------查询编号为1的图书-------------------------------
+# exact 精确的 准确的 就是等于
+BookInfo.objects.get(id__exact=1)
+# 习惯使用下述简写形式
+BookInfo.objects.get(id=1)
+
+# 返回结果：<BookInfo: 射雕英雄传>    --->    get返回的是一个单一对象
+
+# 通过filter获取
+BookInfo.objects.filter(id__exact=1)
+# 返回结果：<QuerySet [<BookInfo: 射雕英雄传>]>    --->    filter返回的是一个列表
+
+# --------------------------------查询书名包含'湖'的图书-------------------------------
+# contains 包含
+BookInfo.objects.filter(name__contains='湖')
+
+# --------------------------------查询书名以'部'结尾的图书-------------------------------
+BookInfo.objects.filter(name__endswith='部')
+
+# --------------------------------查询书名为空的图书-------------------------------
+BookInfo.objects.filter(name__isnull=True)
+
+# --------------------------------查询编号为1或3或5的图书-------------------------------
+BookInfo.objects.filter(id__in=[1, 3, 5])
+
+# --------------------------------查询编号大于3的图书-------------------------------
+# gt    大于
+# gte   大于等于
+# lt    小于
+# lt3    小于等于
+BookInfo.objects.filter(id__gt=3)
+
+# --------------------------------查询书籍id不为3的图书-------------------------------
+BookInfo.objects.exclude(id__exact=3)
+BookInfo.objects.exclude(id=3)
+
+# --------------------------------查询1980年发布的图书-------------------------------
+BookInfo.objects.filter(pub_date__year='1980')
+
+# --------------------------------查询1990年1月1日后发布的图书-------------------------------
+BookInfo.objects.filter(pub_date__gt='1990-1-1')
+# BookInfo.objects.filter(pub_date__gt='1990/1/1')  不正确
