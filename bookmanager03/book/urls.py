e102af1d786918346813a17e9ca7ec9e6e67f108
daykalif <1038200595@qq.com>
2023-01-26 11:03:21 +0800

    feat:add session保存到redis中-增删改

--- bookmanager03/book/urls.py
+++ bookmanager03/book/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from book.views import SetSession
+from book.views import SetSession, GetSession
 
 urlpatterns = [
     url(r'^set_session/', SetSession.as_view()),
+    url(r'^get_session/', GetSession.as_view()),
 ]
--- bookmanager03/book/views.py
+++ bookmanager03/book/views.py
@@ -8,5 +8,29 @@ from django.views import View
 class SetSession(View):
     def get(self, request):
         # request.session = SessionStorage
+        # 增加数据/修改数据
         request.session['name'] = 'itcast'
-        return HttpResponse('abc')
+
+        # 删除某一个数据
+        del request.session['name']
+
+        # 删除session所有数据,会保留key
+        request.session.clear()
+
+        # 把数据库/redis中的key都删除了
+        request.session.flush()
+
+        # session是有时间的，默认是2周，
+        # 我们可以设置时间
+        # request.session.set_expiry(seconds)
+        request.session.set_expiry(10)
+
+        return HttpResponse('set session')
+
+
+class GetSession(View):
+    def get(self, request):
+        # name = request.session['name']    # 当name不存在时，会报错
+        name = request.session.get('name')
+        print('name--->', name)
+        return HttpResponse('get session')
