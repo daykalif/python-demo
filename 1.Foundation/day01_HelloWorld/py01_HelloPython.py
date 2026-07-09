06cbec7bcbd56858cbb9dd7e43e917b19d15411c
daykalif <1038200595@qq.com>
2026-07-09 16:44:58 +0800

    feat: Python 启航

---
+++ 1.Foundation/day01_HelloWorld/py01_HelloPython.py
@@ -0,0 +1,4 @@
+# 需求：往控制台中输出 Hello World,Hello Python (# Python中注释的写法，注释是写给开发人员看的，python解释器会忽略注释 -- 快捷键： command + /)
+print("Hello World")
+print("Hello 你好")
+print("nchar")
--- Foundation/day01_HelloWorld/py01_HelloPython.py
+++
@@ -1,3 +0,0 @@
-print("Hello World")
-print("Hello 你好")
-print("nchar")
